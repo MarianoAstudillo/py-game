a56d9558dfed6fe5350d51683ea45832079e25f8
MarianoAstudillo <astudillo.mariano.0075@gmail.com>
2025-08-14 08:04:08 -0300

    added rotation to player

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
 
         # game section
         player.draw(screen)
