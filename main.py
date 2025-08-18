9df7914450a1fcd31a6bb9e5c352b5c52f60ea71
MarianoAstudillo <astudillo.mariano.0075@gmail.com>
2025-08-18 08:39:43 -0300

    added shots

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 
 def main():
@@ -16,6 +17,7 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -23,8 +25,9 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     
     AsteroidField.containers = updatable
-
     asteroid_field = AsteroidField()
+
+    Shot.containers = (updatable, drawable)
 
     # game loopo
     while True:
---
+++ shot.py
@@ -0,0 +1,14 @@
+from circleshape import CircleShape
+from constants import *
+import pygame
+
+class Shot(CircleShape):
+  def __init__(self, x, y, radius):
+    super().__init__(x, y, radius)
+
+  def draw(self, screen):
+    pygame.draw.circle(screen, "white", self.position, SHOT_RADIUS, 2)
+
+  def update(self, dt):
+    forward = pygame.Vector2(0, 1)
+    self.position += self.velocity * dt
